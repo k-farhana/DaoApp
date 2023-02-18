75e90495a67e82edf3569e97e8d369b86f708914
Samarth Pandey <83112574+Samarth-047@users.noreply.github.com>
2023-02-19 01:47:50 +0530

    Update index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -18,7 +18,7 @@ const Navbar = () => {
       <Nav>
         <Bars />
         <NavMenu>
-          <NavLink to='/user' activeStyle>
+          <NavLink to='/' activeStyle>
             User
           </NavLink>
           <NavLink to='/admin' activeStyle>
