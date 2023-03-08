2f6d026b5d85da2218143edcc763bd084c862f9f
farhana-k <farhana.kotal@gmail.com>
2023-03-08 11:13:11 +0530

    contract

--- frontend/src/contract.js
+++ frontend/src/contract.js
@@ -1,15 +1,25 @@
 import Web3 from "web3";
-import WITDAO from "./WITDAO.json"
+import ISBDAO from "./ISBDAO.json"
+
   const ethereum = window.ethereum;
 
   const web3 = new Web3(ethereum);
 
-  const ContractAddress = '0x9412eaaDc0A5B2883304e5bc022A2971a24076F0';
 
+  // Ganache
+  // const ContractAddress = '0x9412eaaDc0A5B2883304e5bc022A2971a24076F0';
 
+  // Water insight dao
   // admin 0x4E92Aa11959c1417b5b4BA53E98500743d0C0f5d goerli 
   // const ContractAddress = '0xE291427acc23472491E4c6d92DCDd4d0a1ddB7E1';
-  const ContractAbi = WITDAO;
+
+
+  //ISB DAO
+  // admin 
+  // const ContractAddress = '0x76771022787C5Fc5C7162Ca46c414574640Ef0E4';
+
+  const ContractAddress = '0x76771022787C5Fc5C7162Ca46c414574640Ef0E4';
+  const ContractAbi = ISBDAO;
 
   const myContract = new web3.eth.Contract(
     ContractAbi,
