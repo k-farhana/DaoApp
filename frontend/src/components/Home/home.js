cd7287fff47a24faebb912df28fd8707ae22c492
Samarth Pandey <83112574+Samarth-047@users.noreply.github.com>
2023-03-08 10:25:00 +0530

    Add files via upload

--- frontend/src/components/Home/home.js
+++ frontend/src/components/Home/home.js
@@ -6,7 +6,10 @@ import image from "../images/images.png";
 import logo from '../images/isb-logo.png';
 import block from "../images/blockchain-logo.png";
 import nft from "../images/token1.jpeg";
+import sap from "../images/Sap.png";
+import asset from "../images/asset.png";
 import { useNavigate } from "react-router-dom";
+import venture from "../images/venture.png";
 import {
     Nav,
     NavLink,
@@ -15,6 +18,9 @@ import {
     NavBtn,
     NavBtnLink,
 } from '../Navbar/NavbarElements';
+import {
+    Button,
+} from "react-bootstrap";
 
 const Navbar1 = () => {
     const navigate = useNavigate();
@@ -22,15 +28,27 @@ const Navbar1 = () => {
         <>
             {/* <div className='logo-align'><img className='logo' src={logo} alt="logo" /></div> */}
             <Nav>
-                <Bars />
-                <NavMenu>
+                <Bars /> 
+                <div className='top_align_nav1'>
                     <div className='logo-align'><img className='logo' src={logo} alt="logo" /></div>
-                    <div className='Text_product'>Digital Assets Labs</div>
-                    <div className='top_align_nav'>
-                        <div className='About_text'>About</div>
-                        <div className='About_text'>Contact Us</div>
-                    </div>
-                </NavMenu>
+                    {/* <div className='Text_product'>Digital Assets Labs</div> */}
+                    <img className='logo1' src={asset} />
+                    <img className='logo2' src={venture} />
+                </div>
+                <div className='top_align_nav'>
+                    <div className='About_text'>About</div>
+                    <div className='About_text'><span>Contact Us</span></div> 
+                    <div className='About_text'>Refrence Manual</div>   
+                    <div className='About_text'>Team</div> 
+                    <Button
+                            className="btn_back"
+                            
+                        >
+                            Register
+                    </Button>
+                    <div className='pow_text'>Powered by:</div>
+                    <img className='About_logo' src={sap} />
+                </div>
             </Nav>
             <br /><br /><br />
             <div className='home-align'>
